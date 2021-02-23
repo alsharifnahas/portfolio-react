9c31b15bec57c1981da162afb0b0773e34df934e
Alsharif Nahas <eid_alshareef@hotmail.com>
2021-02-22 22:44:50 -0500

    Updates

---
+++ precache-manifest.a1b60e5da2dfa9461b4fffa3319ba717.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60d8870f8c9e2a9aba72d74aef6f3673",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "db33effc1bf9e8454de1",
+    "url": "/portfolio-react/static/css/main.0d2e5193.chunk.css"
+  },
+  {
+    "revision": "bee1a6386a6386751978",
+    "url": "/portfolio-react/static/js/2.ea8108ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio-react/static/js/2.ea8108ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db33effc1bf9e8454de1",
+    "url": "/portfolio-react/static/js/main.ca0c3a57.chunk.js"
+  },
+  {
+    "revision": "cd144baf9fb4c6dd754f",
+    "url": "/portfolio-react/static/js/runtime-main.be285eaa.js"
+  },
+  {
+    "revision": "2407ee086529fb8bb22ff4a370037a5f",
+    "url": "/portfolio-react/static/media/Alsharif-Eid-Nahas_Resume.2407ee08.pdf"
+  },
+  {
+    "revision": "e3f23fb0021c0fa0b1b16ba94018e982",
+    "url": "/portfolio-react/static/media/code-quiz.e3f23fb0.PNG"
+  },
+  {
+    "revision": "eb3c1ec866256f8e83e57f219a6ccb96",
+    "url": "/portfolio-react/static/media/epl-project.eb3c1ec8.PNG"
+  },
+  {
+    "revision": "0d55b663b9ea28fb7bdf5b414ec7df0b",
+    "url": "/portfolio-react/static/media/password-generator.0d55b663.png"
+  },
+  {
+    "revision": "fadb5e22b7869dd5d08ec04cc5b624ef",
+    "url": "/portfolio-react/static/media/recipe-project.fadb5e22.PNG"
+  },
+  {
+    "revision": "03aadd6c29578850a117b9b144fe4d81",
+    "url": "/portfolio-react/static/media/weather-forecast.03aadd6c.PNG"
+  },
+  {
+    "revision": "0727d00ddbac1322f195d347b6ac00a6",
+    "url": "/portfolio-react/static/media/work-day-planner.0727d00d.PNG"
+  }
+]);
